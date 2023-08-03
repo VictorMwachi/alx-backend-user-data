e25788f0aa608c4aaba2050f2d8191e29daac69a
victormwachi <mudakizevictor@gmail.com>
2023-08-03 23:39:56 +0300

    filter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,12 @@ import re
 from typing import List
 
 
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+
+
 def filter_datum(
         fields: List[str], redaction: str, messageseparator: str) -> str:
     """that returns the log message obfuscated"""
