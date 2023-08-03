8797ee56dc25ce12ee95f50a3f5baa3a8943f2d7
Victor Mwachi <mudakizevictor@gmail.com>
2023-08-03 09:34:31 +0000

    test

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,9 @@
 """
 0. Regex-ing
 """
+import re
+from typing import List
 
-def filter_datum(fields,redaction,messageseparator):
-  """that returns the log message obfuscated"""
+
+def filter_datum(fields: List[str], redaction: str, messageseparator: str):
+    """that returns the log message obfuscated"""
