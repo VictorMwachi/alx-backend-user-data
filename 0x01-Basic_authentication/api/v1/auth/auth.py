c7625806d1961cd44ea4ea202caa226090061eab
victormwachi <mudakizevictor@gmail.com>
2023-08-11 23:41:50 +0300

    path

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,15 @@ class Auth:
     """manage api authenticate"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """return none"""
-        return None
+        if path is None:
+            return True
+        if exclude_paths is None:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in exclude_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """return none"""
