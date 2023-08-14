69ecaac4fb428da035959e3da4338e5f355daed3
victormwachi <mudakizevictor@gmail.com>
2023-08-14 22:55:09 +0300

    db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,8 +6,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base
-from typing import Object
+from user import Base, User
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class DB:
@@ -31,7 +32,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password) -> Object:
+    def add_user(self, email, hashed_password) -> User:
         """returns user object"""
         self.email = email
         self.hashed_password = hashed_password
@@ -44,3 +45,35 @@ class DB:
             self.__session.rollback()
             self.__session = None
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """returns the first row found in the users
+        table as filtered by the method’s input arguments"""
+        query_dict = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                query_dict[key] = kwargs[key]
+            else:
+                raise InvalidRequestError()
+        result = self.__session.query(User).filter_by(**query_dict).first()
+        if result is None:
+            raise NoResultFound()
+        return result
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates a user based on a given id.
+        """
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        update_dict = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                update_dict[key] = kwargs[key]
+            else:
+                raise ValueError()
+        self._session.query(User).filter(User.id == user_id).update(
+            update_dict,
+            synchronize_session=False,
+        )
+        self._session.commit()
