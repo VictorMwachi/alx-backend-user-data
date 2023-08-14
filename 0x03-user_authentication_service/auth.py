06b3944038674ff7208dcf4bab8f8a1926d5e895
victormwachi <mudakizevictor@gmail.com>
2023-08-14 23:40:10 +0300

    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

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""4. Hash password"""
+import bcrypt
+from db import DB
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password.
+    """
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """add new user to db"""
+
